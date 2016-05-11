499fe7229f22484b23914280ed48f3e57bd9d6c0
Dan <danvolosnikov@gmail.com>
2016-05-11 19:16:52 +0000

    sm ch

--- app/model/latest.js
+++ app/model/latest.js
@@ -4,12 +4,12 @@ var api = require('../api/index.js');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 module.exports = function (app) {
-    var historySchema = new Schema({
+    var recent = new Schema({
     term: String,
     when: String
 });
 
-    var History = mongoose.model('History',historySchema);
+    var History = mongoose.model('History',recent);
 
     mongoose.connect(process.env.MONGODB_URI);
     api(app,History);
